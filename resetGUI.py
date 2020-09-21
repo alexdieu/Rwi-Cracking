0b5ffd37db6c8f43d87d120e3e5d59fc390bc5dd
Alexdieu <alexdieuveult@gmail.com>
2020-09-21 23:41:39 +0200

    Add files via upload

---
+++ resetGUI.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import *
+import shutil
+
+def reset():
+  print("patching")
+  shutil.rmtree('AppData')
+
+root = Tk()
+
+root.geometry('800x500')
+root.configure(background='#F0F8FF')
+root.title('RWI RESETER')
+
+Label(root, text='RWI RESETER', bg='#F0F8FF', font=('courier', 48, 'bold')).place(x=168, y=44)
+
+Label(root, text='This will reset RWI editor , including trial days , ONLY WORKING WITH PORTABLE VERSION', bg='#F0F8FF', font=('helvetica', 12, 'bold')).place(x=38, y=101)
+
+Button(root, text='RESET', bg='#FF0000', font=('courier', 48, 'italic'), command=reset).place(x=247, y=180)
+
+
+root.mainloop()
